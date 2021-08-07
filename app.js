e67633f3f7f2fbbeeac8d047d62eb932f8c6f5ea
fahadaleem <faleem396@gmail.com>
2021-08-07 22:16:15 +0500

    Dynamic Classes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 new Vue({
     el: '#app',
     data:{
-        name: 'Fahad Aleem',
+        name: '',
         job: 'Software Engineer',
         window:"https://www.facebook.com/",
         windowTag:'<a href="https://www.facebook.com/">Click Here</a>',
-        age: 20,
+        age:'',
+        a:0,
+        b:0,
         },
     methods:{
         greet:function(time){
@@ -16,6 +18,25 @@ new Vue({
         },
         decrementAge: function(value){
             this.age -= value;
+        },
+        // addToA:function(){
+        //     console.log("addToA");
+        //     this.a += this.age;
+        // },
+        // addToB:function(){
+        //     console.log("addToB");
+        //     this.b += this.age;
+        // }
+
+    },
+    computed:{
+        addToA:function(){
+            console.log("addToA");
+            this.a += this.age;
+        },
+        addToB:function(){
+            console.log("addToB");
+            this.b += this.age;
         }
     }
 });
