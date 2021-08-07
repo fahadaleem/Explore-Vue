e8b0f3437730c80b24ef8d68209ce20b6e2d1382
fahadaleem <faleem396@gmail.com>
2021-08-07 21:28:56 +0500

    Events

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
         name: 'Fahad Aleem',
         job: 'Software Engineer',
         window:"https://www.facebook.com/",
-        windowTag:'<a href="https://www.facebook.com/">Click Here</a>'
+        windowTag:'<a href="https://www.facebook.com/">Click Here</a>',
+        age: 20,
         },
     methods:{
         greet:function(time){
